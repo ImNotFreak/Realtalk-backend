2b612bffe69476bc3942e36f9c742639ac920ff1
alex <rybakov.alexsandr28@gmail.com>
2025-09-02 15:18:47 +0300

    Добавлены базовые зависимости

---
+++ real-talk/src/main/java/real/talk/RealTalkApplication.java
@@ -0,0 +1,13 @@
+package real.talk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTalkApplication.class, args);
+    }
+
+}
